2ac88324847ee14e55f9272bc26e0c1b0111fa4c
marvin <mcanastuj1999@gmail.com>
2026-07-25 17:29:44 -0600

    adding part of learning activity

--- src/models/organizations.js
+++ src/models/organizations.js
@@ -16,4 +16,22 @@ const getAllOrganizations = async () => {
     return result.rows;
 }
 
-export { getAllOrganizations }
+const getOrganizationDetails = async (organizationId) => {
+    const query = `
+        SELECT
+            organization_id,
+            organization_name,
+            description,
+            contact_email,
+            logo
+        FROM organization
+        WHERE organization_id = $1
+    `
+
+    const queryParams = [organizationId]
+    const result = await db.query(query, queryParams)
+
+    return result.rows.length > 0 ? result.rows[0] : null;
+}
+
+export { getAllOrganizations, getOrganizationDetails }
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import { showOrganizationsPage } from './controllers/organizations.js'
 import { showProjectsPage } from './controllers/projects.js'
 import { showCategoriesPage } from './controllers/categories.js'
 import { showTestErrorPage } from './controllers/errors.js'
+import { showOrganizationDetailsPage } from './controllers/organizations.js'
 
 const router = express.Router()
 
@@ -12,7 +13,9 @@ router.get('/', showHomePage)
 router.get('/organizations', showOrganizationsPage)
 router.get('/projects',  showProjectsPage)
 router.get('/categories', showCategoriesPage)
+router.get('/organization/:id', showOrganizationDetailsPage)
 // Test route for 500 errors
 router.get('/test-error', showTestErrorPage)
+
 
 export default router
